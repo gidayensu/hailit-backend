df60ad3aff7ccc12af1248a90a970645a0d0d2b9
giadyensukwabi <thurgid@gmail.com>
2024-07-05 02:11:34 +0000

    Added total items to response to paginatedRequests

--- src/utils/paginatedRequest.js
+++ src/utils/paginatedRequest.js
@@ -7,12 +7,14 @@ export const paginatedRequest = async (
   ) => {
     const {total_count} = totalCount;
     const total_number_of_pages = Math.floor(total_count / limit);
-    const current_page = offset / limit;
+
+    const current_page = (offset / limit)+1;
   
     return {
       [source]: data,
       total_number_of_pages,
-      current_page
+      current_page,
+      total_items: total_count
     };
   };
   
